384d93445b76e490e4d797bc9a8a01b4bf1e6583
Amaranque <64761805+IdiosyncraticError@users.noreply.github.com>
2026-05-01 16:19:20 -0400

    file cleanup
    moved apworld files to a folder, added json generator code bcs ts pmo

---
+++ data_generation.py
@@ -0,0 +1,43 @@
+upgrades = [
+    ["Adrenaline", "1", "Solo"],
+    ["Swiftness Ring", "3", ""],
+    ["Business License", "2", ""],
+    ["Paycheck", "5", ""],
+    ["Orb", "1", "Casual"],
+    ["Medal", "1", ""],
+    ["Radar", "1", ""],
+    ["Better Jump Pads", "1", ""],
+    ["Tria Orbs", "1", ""],
+    ["Grapple Points", "1", ""],
+    ["Double Jump", "1", "", 4],
+    ["Defuse Kit", "3", "Standard+"],
+    ["Grace Wings", "1", "", 4],
+    ["Pocket Bell", "1", "", 4],
+    ["Larger Grapple Points", "1", ""],
+    ["Ice Skates", "1", ""],
+    ["Helmet", "1", ""],
+    ["Fanny Pack", "1", ""],
+    ["Advanced Gravity Coil", "1", ""],
+    ["More Altars", "1", ""],
+    ["Last Man Standing", "1", "Multiplayer"],
+    ["Radar Module: Enemies", "1", ""],
+    ["Radar Module: Altars", "1", ""],
+    ["Radar Module: Players", "1", "Multiplayer"],
+    ["Radar Module: Tripmines", "1", "Standard+"],
+    ["Ninja Belt", "1", "", 4],
+    ["Subspacial Barrier", "2", "Standard+"],
+    ["Shark Tail", "1", ""],
+    ["Gift Magnet", "3", ""],
+    ["Matrix Tetrahedron", "1", ""],
+    ["Shield", "1", ""],
+    ["Radar Module: Instruments", "1", ""],
+    ["Panic Necklace", "1", ""],
+    ["Miniature Hourglass", "1", ""],
+    ["Gift Idol", "5", ""],
+    ["Drowned Aegis", "1", ""]
+]
+
+output = ""
+
+#with open("data.json", "w") as file:
+#    json.dump(output, file, indent=4)
