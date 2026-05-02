3592c8b634862e183d9181c87ce189aef68a65f7
Amaranque <64761805+IdiosyncraticError@users.noreply.github.com>
2026-05-01 21:23:11 -0400

    updated location gen
    added regions and categories to location script, added categories to location json

--- location_json_gen.py
+++ location_json_gen.py
@@ -1,93 +1,93 @@
 import json
 
 enemies = [
-    ["Bell", ["Level 1+"]],
-    ["Mart", ["Level 1+"]],
-    ["Baby", ["Level 1+"]],
-    ["Springer", ["Level 1+"]],
-    ["Husk", ["Level 1+"]],
-    ["ICBM", ["Level 1+"]],
-    ["Flesh", ["Level 5+"]],
-    ["NIL", ["Level 5+"]],
-    ["Operator", ["Level 5+"]],
-    ["Guardian", ["Level 8+"]],
-    ["Telefragger", ["Level 8+"]],
-    ["Kolona", ["Level 10+"]],
-    ["Voidbreaker", ["Level 15+"]],
-    ["Cadence", ["Level 15+"]],
-    ["Voidbound Baby", ["Level 10+"]],
-    ["Voidbound Guardian", ["Level 20+"]],
-    ["Scrapmaw", ["Level 25+"]]
+    ["Bell", [1]],
+    ["Mart", [1]],
+    ["Baby", [1]],
+    ["Springer", [1]],
+    ["Husk", [1]],
+    ["ICBM", [1]],
+    ["Flesh", [5]],
+    ["NIL", [5]],
+    ["Operator", [5]],
+    ["Guardian", [8]],
+    ["Telefragger", [8]],
+    ["Kolona", [10]],
+    ["Voidbreaker", [15]],
+    ["Cadence", [15]],
+    ["Voidbound Baby", [10]],
+    ["Voidbound Guardian", [20]],
+    ["Scrapmaw", [25]]
     #["???", [""]]
 ]
 
 curses = [
-    ["Lower Gravity", ["Level 1+"]],
-    ["Random Spawn", ["Level 1+"]],
-    ["Savory Ring", ["Level 5+"]],
-    ["Scattered Gifts", ["Level 5+"]],
-    ["Weaker Jump Pads", ["Level 1+"]],
-    ["Beacon Mirage", ["Level 25+"]],
-    ["Bigger Tripmines", ["Level 5+"]],
-    ["High Roller", ["Level 5+"]],
-    ["Tweaked Odds", ["Level 5+"]],
-    ["Jackpot", ["Level 12+"]], #level unconfirmed
-    ["Lap 2", ["Level 8+"]],
-    ["Fragile Gifts", ["Level 8+"]],
-    ["More Tripmines", ["Level 5+"]],
-    ["Fake Count", ["Level 8+"]],
-    ["Nothing?", ["Level 8+"]],
-    ["Barotrauma", ["Level 15+"]],
-    ["Minefield", ["Level 15+"]],
+    ["Lower Gravity", [1]],
+    ["Random Spawn", [1]],
+    ["Savory Ring", [5]],
+    ["Scattered Gifts", [5]],
+    ["Weaker Jump Pads", [1]],
+    ["Beacon Mirage", [25]],
+    ["Bigger Tripmines", [5]],
+    ["High Roller", [5]],
+    ["Tweaked Odds", [5]],
+    ["Jackpot", [12]], #level unconfirmed
+    ["Lap 2", [8]],
+    ["Fragile Gifts", [8]],
+    ["More Tripmines", [5]],
+    ["Fake Count", [8]],
+    ["Nothing?", [8]],
+    ["Barotrauma", [15]],
+    ["Minefield", [15]],
 
-    ["More Ringing", ["Level 1+", "Bell"]],
-    ["Mighty Gong", ["Level 1+", "Bell"]],
-    ["Concussion", ["Level 1+", "Bell"]],
-    ["Mart Infection", ["Level 1+", "Mart"]],
-    ["Mart Slide", ["Level 1+", "Mart"]],
-    ["Bigger Marts", ["Level 1+", "Mart"]],
-    ["Closer Husk", ["Level 1+", "Husk"]],
-    ["Further Husk", ["Level 1+", "Husk"]],
-    ["Taller Husk", ["Level 1+", "Husk"]],
-    ["Random Husk", ["Level 15+", "Husk"]],
-    ["Husk Express", ["Level 8+", "Husk"]],
-    ["Conga Line", ["Level 10+", "Husk"]],
-    ["Springloaded", ["Level 5+", "Springer"]],
-    ["Resonating Shockwaves", ["Level 1+", "Springer"]],
-    ["Ambush", ["Level 8+", "Telefragger"]],
-    ["Accurate Telefragger", ["Level 8+", "Telefragger"]],
-    ["Bloodier Meat", ["Level 5+", "Flesh"]],
-    ["Blighted Jump Pads", ["Level 5+", "Flesh"]],
-    ["Cognitive Dissonance", ["Level 5+", "NIL"]],
-    ["Bigger Blast", ["Level 5+", "ICBM"]],
-    ["Scorched Earth", ["Level 1+", "ICBM"]],
-    ["Pacifier", ["Level 1+", "Baby"]],
-    ["Problem Child", ["Level 5+", "Baby"]],
-    ["Shotgun", ["Level 5+", "Guardian"]],
-    ["Camouflage", ["Level 5+", "Guardian"]],
-    ["Burning Bouquet", ["Level 10+", "Kolona"]],
-    ["Lost Embers", ["Level 10+", "Kolona"]],
-    ["Blade Carousel", ["Level 15+", "Voidbreaker"]],
-    ["Deadly Melody", ["Level 15+", "Cadence"]],
-    ["Blueprint: Cross Beams", ["Level 25+", "Scrapmaw"]],
+    ["More Ringing", [1, "Bell"]],
+    ["Mighty Gong", [1, "Bell"]],
+    ["Concussion", [1, "Bell"]],
+    ["Mart Infection", [1, "Mart"]],
+    ["Mart Slide", [1, "Mart"]],
+    ["Bigger Marts", [1, "Mart"]],
+    ["Closer Husk", [1, "Husk"]],
+    ["Further Husk", [1, "Husk"]],
+    ["Taller Husk", [1, "Husk"]],
+    ["Random Husk", [15, "Husk"]],
+    ["Husk Express", [8, "Husk"]],
+    ["Conga Line", [10, "Husk"]],
+    ["Springloaded", [5, "Springer"]],
+    ["Resonating Shockwaves", [1, "Springer"]],
+    ["Ambush", [8, "Telefragger"]],
+    ["Accurate Telefragger", [8, "Telefragger"]],
+    ["Bloodier Meat", [5, "Flesh"]],
+    ["Blighted Jump Pads", [5, "Flesh"]],
+    ["Cognitive Dissonance", [5, "NIL"]],
+    ["Bigger Blast", [5, "ICBM"]],
+    ["Scorched Earth", [1, "ICBM"]],
+    ["Pacifier", [1, "Baby"]],
+    ["Problem Child", [5, "Baby"]],
+    ["Shotgun", [5, "Guardian"]],
+    ["Camouflage", [5, "Guardian"]],
+    ["Burning Bouquet", [10, "Kolona"]],
+    ["Lost Embers", [10, "Kolona"]],
+    ["Blade Carousel", [15, "Voidbreaker"]],
+    ["Deadly Melody", [15, "Cadence"]],
+    ["Blueprint: Cross Beams", [25, "Scrapmaw"]],
 ]
 
 greater_curses = [
-    ["Trap Card", ["Level 15+"]],
-    ["Run", ["Level 15+"]],
-    ["One Less Choice", ["Level 10+"]],
-    ["Inverse Destruction", ["Level 15+"]],
-    ["Void Implosions", ["Level 10+"]],
-    ["Oblivion", ["Level 10+"]],
-    ["Razorbloom", ["Level 10+"]],
+    ["Trap Card", [15]],
+    ["Run", [15]],
+    ["One Less Choice", [10]],
+    ["Inverse Destruction", [15]],
+    ["Void Implosions", [10]],
+    ["Oblivion", [10]],
+    ["Razorbloom", [10]],
 
-    ["Tantrum", ["Level 10+", "Baby"]],
-    ["Hollow Tiles", ["Level 10+", "ICBM"]],
-    ["Destroyer", ["Level 15+", "Husk"]],
-    ["Mass Infection", ["Level 10+", "Flesh"]],
-    ["Ballet of Blades", ["Level 15+", "Voidbreaker"]],
-    ["Blade Bombardment", ["Level 15+", "Voidbreaker"]],
-    ["Malfunction", ["Level 10+", "Operator"]],
+    ["Tantrum", [10, "Baby"]],
+    ["Hollow Tiles", [10, "ICBM"]],
+    ["Destroyer", [15, "Husk"]],
+    ["Mass Infection", [10, "Flesh"]],
+    ["Ballet of Blades", [15, "Voidbreaker"]],
+    ["Blade Bombardment", [15, "Voidbreaker"]],
+    ["Malfunction", [10, "Operator"]],
 ]
 
 rounds = [
@@ -98,6 +98,16 @@ output = []
 for i in rounds:
     obj = {}
     obj["name"] = "Level " + str(i)
+    if i < 10:
+        obj["region"] = "lvl 0"
+    elif i < 15:
+        obj["region"] = "lvl 10"
+    elif i < 20:
+        obj["region"] = "lvl 15"
+    elif i < 25:
+        obj["region"] = "lvl 20"
+    else:
+        obj["region"] = "lvl 25"
     obj["category"] = "Levels"
     output.append(obj)
 
@@ -105,7 +115,18 @@ for i in enemies:
     obj = {}
     obj["name"] = i[0]
     
-    cat_list = i[1]
+    if i[1][0] < 10:
+        obj["region"] = "lvl 0"
+    elif i[1][0] < 15:
+        obj["region"] = "lvl 10"
+    elif i[1][0] < 20:
+        obj["region"] = "lvl 15"
+    elif i[1][0] < 25:
+        obj["region"] = "lvl 20"
+    else:
+        obj["region"] = "lvl 25"
+    
+    cat_list = ["Level " + str(i[1][0]) + "+"]
     cat_list.append("Enemies")
     obj["category"] = cat_list
     output.append(obj)
@@ -113,7 +134,22 @@ for i in enemies:
 for i in curses:
     obj = {}
     obj["name"] = i[0]
-    cat_list = i[1]
+
+    if i[1][0] < 10:
+        obj["region"] = "lvl 0"
+    elif i[1][0] < 15:
+        obj["region"] = "lvl 10"
+    elif i[1][0] < 20:
+        obj["region"] = "lvl 15"
+    elif i[1][0] < 25:
+        obj["region"] = "lvl 20"
+    else:
+        obj["region"] = "lvl 25"
+
+    cat_list = []
+    cat_list.append("Level " + str(i[1][0]) + "+")
+    if len(i[1]) > 1:
+        cat_list.append(i[1][1]) #will need to change to accomodate more categories in the future but this is fine for now
     cat_list.append("Curses")
     obj["category"] = cat_list
     output.append(obj)
@@ -121,7 +157,22 @@ for i in curses:
 for i in greater_curses:
     obj = {}
     obj["name"] = i[0]
-    cat_list = i[1]
+
+    if i[1][0] < 10:
+        obj["region"] = "lvl 0"
+    elif i[1][0] < 15:
+        obj["region"] = "lvl 10"
+    elif i[1][0] < 20:
+        obj["region"] = "lvl 15"
+    elif i[1][0] < 25:
+        obj["region"] = "lvl 20"
+    else:
+        obj["region"] = "lvl 25"
+
+    cat_list = []
+    cat_list.append("Level " + str(i[1][0]) + "+")
+    if len(i[1]) > 1:
+        cat_list.append(i[1][1]) #will need to change to accomodate more categories in the future but this is fine for now
     cat_list.append("Greater Curses")
     obj["category"] = cat_list
     output.append(obj)
